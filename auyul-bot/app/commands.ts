c07f21677017a80eed0221566ec3aab5b9312950
황승원 <kevin1113dev@gmail.com>
2024-04-26 21:12:28 +0900

    음악 재생 명령어 추가

--- auyul-bot/app/commands.ts
+++ auyul-bot/app/commands.ts
@@ -1,42 +1,103 @@
-import { ChannelType, SlashCommandBuilder } from 'discord.js';
+import { ChannelType, SlashCommandBuilder } from "discord.js";
 
-const Commands: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">[] = [
+const Commands:
+  | SlashCommandBuilder
+  | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">[] = [
   new SlashCommandBuilder()
-    .setName('들어와')
-    .setDescription('음성채널에 참가합니다.'),
-
-  new SlashCommandBuilder()
-    .setName('나가')
-    .setDescription('음성채널에서 나갑니다.'),
+    .setName("들어와")
+    .setDescription("음성채널에 참가합니다."),
 
   new SlashCommandBuilder()
-    .setName('채널설정')
-    .setDescription('명령어 채널을 설정합니다.')
-    .addChannelOption(option =>
-      option.setName('채널')
-        .setDescription('음악 명령어 채널')
+    .setName("나가")
+    .setDescription("음성채널에서 나갑니다."),
+
+  new SlashCommandBuilder()
+    .setName("채널설정")
+    .setDescription("명령어 채널을 설정합니다.")
+    .addChannelOption((option) =>
+      option
+        .setName("채널")
+        .setDescription("음악 명령어 채널")
         .addChannelTypes(ChannelType.GuildText)
-        .setRequired(true)),
-  
-  new SlashCommandBuilder()
-    .setName('채널해제')
-    .setDescription('명령어 채널을 해제합니다.'),
-      
-  new SlashCommandBuilder()
-    .setName('음소거')
-    .setDescription('아율봇이 채팅을 치지 않도록 음소거합니다.'),
-  
-  new SlashCommandBuilder()
-    .setName('음소거해제')
-    .setDescription('아율봇의 음소거를 해제합니다.'),
+        .setRequired(true)
+    ),
 
   new SlashCommandBuilder()
-    .setName('리모컨')
-    .setDescription('조작이 손쉬운 리모컨 웹 페이지 링크를 불러옵니다.'),
+    .setName("채널해제")
+    .setDescription("명령어 채널을 해제합니다."),
 
   new SlashCommandBuilder()
-    .setName('도움말')
-    .setDescription('도움말을 불러옵니다.'),
+    .setName("음소거")
+    .setDescription("아율봇이 채팅을 치지 않도록 음소거합니다."),
+
+  new SlashCommandBuilder()
+    .setName("음소거해제")
+    .setDescription("아율봇의 음소거를 해제합니다."),
+
+  new SlashCommandBuilder()
+    .setName("리모컨")
+    .setDescription("조작이 손쉬운 리모컨 웹 페이지 링크를 불러옵니다."),
+
+  new SlashCommandBuilder()
+    .setName("도움말")
+    .setDescription("도움말을 불러옵니다."),
+
+  new SlashCommandBuilder()
+    .setName("검색")
+    .setDescription("노래를 검색합니다.")
+    .addStringOption((option) =>
+      option.setName("검색어").setDescription("검색어").setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName("재생")
+    .setDescription("노래를 재생합니다."),
+
+  new SlashCommandBuilder()
+    .setName("일시정지")
+    .setDescription("노래를 일시정지합니다."),
+
+  new SlashCommandBuilder()
+    .setName("이전곡")
+    .setDescription("이전 곡을 재생합니다."),
+  
+  new SlashCommandBuilder()
+    .setName("다음곡")
+    .setDescription("다음 곡을 재생합니다."),
+
+  new SlashCommandBuilder()
+    .setName("정지")
+    .setDescription("노래를 정지합니다."),
+
+  new SlashCommandBuilder()
+    .setName("목록")
+    .setDescription("재생목록을 불러옵니다."),
+
+  new SlashCommandBuilder()
+    .setName("제거")
+    .setDescription("재생목록에서 노래를 제거합니다.")
+    .addIntegerOption((option) =>
+      option.setName("번호").setDescription("제거할 노래의 번호").setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName("섞기")
+    .setDescription("재생목록을 섞습니다."),
+
+  new SlashCommandBuilder()
+    .setName("반복")
+    .setDescription("노래를 반복재생합니다."),
+
+  new SlashCommandBuilder()
+    .setName("반복해제")
+    .setDescription("노래 반복재생을 해제합니다."),
+
+  new SlashCommandBuilder()
+    .setName("볼륨")
+    .setDescription("볼륨을 조절합니다.")
+    .addIntegerOption((option) =>
+      option.setName("볼륨").setDescription("조절할 볼륨").setRequired(true)
+    ),
 ];
 
 export default Commands;
