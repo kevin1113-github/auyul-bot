cb77294af6fd9189e37390b909faad23a124fbc1
황승원 <kevin1113dev@gmail.com>
2024-08-16 15:43:56 +0900

    Update api.ts

--- auyul-bot/app/api.ts
+++ auyul-bot/app/api.ts
@@ -1,8 +1,8 @@
-import { Client } from "discord.js";
+import { Client, EmbedBuilder } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
-const REQUEST_PASSWORD: string = process.env.REQUEST_PASSWORD ?? '';
-
+const REQUEST_PASSWORD: string = process.env.REQUEST_PASSWORD ?? "";
+const DEV_MODE: boolean = process.env.DEV_MODE === "true" ? true : false;
 
 import http from "http";
 import { Servers } from "./dbObject.js";
@@ -12,17 +12,19 @@ export default class HttpServer {
   private server: http.Server;
   private client: Client;
 
-  private async notice(data: string) {
+  private async notice(data: EmbedBuilder) {
     await Servers.sync();
     const servers: T_DATA[] = await Servers.findAll();
     for (const server of servers) {
-      const ttsChannel = server.dataValues.ttsChannel;
-      // if(ttsChannel && server.dataValues.id == '1215573434159996948') {
-      if(ttsChannel) {
-        this.client.channels.fetch(ttsChannel).then(channel => {
-          if(channel?.isTextBased()){
+      if (DEV_MODE && server.dataValues.id != "1233212899862908938") {
+        continue;
+      }
+      const commandChannel = server.dataValues.commandChannel;
+      if (commandChannel) {
+        this.client.channels.fetch(commandChannel).then((channel) => {
+          if (channel?.isTextBased()) {
             try {
-              channel.send(data);
+              channel.send({ embeds: [data] });
             } catch (error) {
               console.log(error);
             }
@@ -32,7 +34,10 @@ export default class HttpServer {
     }
   }
 
-  private requestHandler = (req: http.IncomingMessage, res: http.ServerResponse) => {
+  private requestHandler = (
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) => {
     if (req.url === "/notice" && req.method === "POST") {
       let postData: string = "";
       req.on("data", (data) => {
@@ -41,34 +46,35 @@ export default class HttpServer {
       req.on("end", async () => {
         const password = postData.split(",")[0];
         postData = postData.split(",")[1];
-        if (password.startsWith("password=") && password.split("=")[1] === REQUEST_PASSWORD) {
-          await this.notice(postData);
+        postData = postData.replace(/\\n/g, "\n");
+        const title: string = postData.split("\n")[0];
+        postData = postData.replace(title + "\n", "");
+        if (
+          password.startsWith("password=") &&
+          password.split("=")[1] === REQUEST_PASSWORD
+        ) {
+          const embed = new EmbedBuilder()
+            .setColor("#9A8ED7")
+            .setTitle(title)
+            .setDescription(postData)
+            .setFooter({
+              text: "아율봇 ⓒ 2024. @kevin1113dev All Rights Reserved.",
+              iconURL:
+                "https://github.com/kevin1113-github/auyul-bot/blob/master/auyul-profile.png?raw=true",
+            });
+
+          await this.notice(embed);
         }
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write("OK");
         res.end();
       });
-    }
-
-    // else if (req.url === "/controller" && req.method === "GET") {
-
-    //   res.writeHead(200, { "Content-Type": "text/html" });
-    //   res.write("<h1>Controller</h1>");
-    //   res.write("<form action='/notice' method='post'>");
-    //   res.write("<input type='text' name='password' placeholder='password' />");
-    //   res.write("<input type='text' name='data' placeholder='data' />");
-    //   res.write("<input type='submit' />");
-    //   res.write("</form>");
-    //   res.end();
-
-    // }
-
-    else {
+    } else {
       res.writeHead(404, { "Content-Type": "text/html" });
       res.write("<h1>404 Not Found</h1>");
       res.end();
     }
-  }
+  };
 
   constructor(client: Client) {
     this.server = http.createServer(this.requestHandler);
