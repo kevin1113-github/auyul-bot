3f131c92bb82bab44811cb2341a20c11f6e6b29b
황승원 <kevin1113dev@gmail.com>
2024-08-16 12:11:07 +0900

    cookie setting

--- auyul-bot/app/ytdl.ts
+++ auyul-bot/app/ytdl.ts
@@ -1,52 +1,25 @@
 import { AudioResource, createAudioResource } from "@discordjs/voice";
 // import ytdl from "ytdl-core";
 import ytdl from "@distube/ytdl-core";
-// import fs from "fs";
+import fs from "fs";
 
-// const ytdl_agent = ytdl.createAgent(JSON.parse(fs.readFileSync("cookies.json").toString()));
-// console.log("ytdl agent created", JSON.parse(fs.readFileSync("cookies.json").toString()));
-// console.log(ytdl_agent.jar, ytdl_agent.localAddress);
-
-// const { getRandomIPv6 } = require("@distube/ytdl-core/lib/utils");
-// import { getRandomIPv6 } from "@distube/ytdl-core/lib/utils.js";
-
-const agentIP = ytdl.createAgent(undefined, {
-  // localAddress: getRandomIPv6("2406:da12:2bf:b700::/56"),
-  localAddress: "2406:da12:2bf:b7bb:7dc8:b0ad:7014:2849",
-});
-
-/*
-const agentOptions = {
-  headers: {
-    referer: 'https://www.youtube.com/',
-  },
-};
-*/
-
-// const proxyAgent = ytdl.createProxyAgent({ uri: "http://152.26.229.66:9443" });
+const cookies = JSON.parse(fs.readFileSync("cookies.json").toString());
+const agent = ytdl.createAgent(cookies);
+console.log("ytdl agent created:", cookies);
 
 function ytdlmusic (url: string) {
   return ytdl(url, {
+    agent: agent,
     filter: "audioonly",
     quality: "highestaudio",
     dlChunkSize: 0,
     begin: 0,
     highWaterMark: 1 << 25,
-    agent: agentIP,
-    // agent: proxyAgent,
-    // IPv6Block: "2406:da12:2bf:b700::/56",
   });
 }
 
 export function ytdlAudioResource (url: string) {
-  // console.log("ytdlAudioResource", url);
-  
   const stream = ytdlmusic(url);
-  /*
-  stream.on("data", (chunk) => {
-    console.log("stream data", chunk.length);
-  });
-  */
   const audioResource: AudioResource = createAudioResource(stream);
   return audioResource;
 }
