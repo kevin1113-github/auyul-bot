ef8d01899fc7527e3251cde3fe808af2e742633a
kevin <kevin1113dev@gmail.com>
2025-06-13 22:53:13 +0900

    .

--- juhee-bot/app/types.ts
+++
@@ -1,6 +0,0 @@
-import { Model } from "sequelize";
-import { AudioPlayer } from '@discordjs/voice';
-import Action from "./action.js";
-
-export type DATA = Model<any, any>;
-export type GuildData = { guildId: string, audioPlayer: AudioPlayer | null, action: Action, timeOut: NodeJS.Timeout | null };
